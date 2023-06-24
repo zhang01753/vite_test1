26ffcba364dd9a0ca10f90b92f439692cbd81dc5
zhang01753 <luna1210zz@outlook.com>
2023-06-24 09:35:58 +0800

    更新

--- src/main.js
+++ src/main.js
@@ -11,6 +11,38 @@ import router from './routers/index';
 
 const app = createApp(App);
 
+// //获取线索
+// async getClue () {
+//     if(this.query.isRetateClue) {
+//         const res = await this.$ajax.sales.fetchClueList({
+//             queryType: 6,
+//             pageNo: 1,
+//             pageSize: 150,
+//             groupID: this.query.customerID,
+//         })
+
+//         if (res?.success) {
+//             const [ clueInfo = {} ] = res?.data?.data ?? []
+//             const { address, contactMobile, storeName: shopName } = clueInfo
+//             const formInfo = {
+//                 shopName,
+//                 address,
+//                 contactMobile,
+//             }
+
+//             this.formInfo = {
+//                 ...this.formInfo,
+//                 ...formInfo,
+//             }
+//         }
+//     }
+// },
+
+// //基于客户id获取线索列表
+// fetchClueList: (data) => ajax({
+//     url: '/aaa/aaa',
+//     data,
+// }),
 
 app.use(Antd).mount('#app');
 // 5. 创建并挂载根实例
